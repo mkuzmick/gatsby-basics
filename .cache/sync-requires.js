bd05bc3ed8642582461285a8f920c3f0a90e4179
mk <mk@mks-MacBook-Air.local>
2019-11-13 07:17:36 -0500

    Updates:

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,8 +5,7 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/mk/Development/gatsby-basics/.cache/dev-404-page.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/Users/mk/Development/gatsby-basics/src/pages/index.js"))),
-  "component---src-pages-about-js": hot(preferDefault(require("/Users/mk/Development/gatsby-basics/src/pages/about.js")))
+  "component---src-pages-about-js": hot(preferDefault(require("/Users/mk/Development/gatsby-basics/src/pages/about.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/mk/Development/gatsby-basics/src/pages/index.js")))
 }
 
