0be53dbb2c2ac983f6b74dc22489b2407a5484a0
mk <mk@mks-MacBook-Air.local>
2019-11-13 07:18:36 -0500

    Updates

--- .cache/async-requires.js
+++
@@ -1,8 +0,0 @@
-// prefer default export if available
-const preferDefault = m => m && m.default || m
-
-exports.components = {
-  "component---src-pages-about-js": () => import("../src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */),
-  "component---src-pages-index-js": () => import("../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
-}
-
--- src/components/header.js
+++
@@ -1,5 +0,0 @@
-import React from "react"
-
-export default (props) => (
-  <h1 style={{ fontFamily: `Avenir Next, sans-serif`, fontWeight: `900` }}>{props.headerText}</h1>
-)
