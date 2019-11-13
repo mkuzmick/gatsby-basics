e2422cdca19e71e33a891866177be2c33b2ff6f7
mk <mk@mks-macbook-air.lan>
2019-11-12 22:29:35 -0500

    gh-pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import { Link } from "gatsby"
 export default () => (
   <div style={{ color: `teal`, fontFamily: `Avenir, sans-serif` }}>
     <Link to="/about">About</Link>
-    <Header headerText="Work in Progess" />
+    <Header headerText="Work in Progress" />
     <p>work in progress will happen here.</p>
   </div>
 )
